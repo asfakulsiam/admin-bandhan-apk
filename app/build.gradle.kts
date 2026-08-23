16dda7ff8089c9ff7bb0f2ddd54e374a3b2d08ca
Asfakul Islam <116263975+asfakulsiam@users.noreply.github.com>
2026-08-23 23:37:58 +0600

    Update versionCode and versionName in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.admin.bandhan17.app"
     minSdk = 24
     targetSdk = 36
-    versionCode = 26
-    versionName = "2.2.18"
+    versionCode = 27
+    versionName = "2.2.19"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
